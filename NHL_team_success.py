e018b42f5d30382e5c06508cc77c80d7d9516f9b
jonah550 <jonahbot@umich.edu>
2024-12-08 22:48:14 -0500

    Penalty vs points graph commit 2

---
+++ NHL_team_success.py
@@ -0,0 +1,28 @@
+import unittest
+import sqlite3
+import json
+import os
+import numpy as np
+from scipy import stats
+
+import matplotlib.pyplot as plt
+
+
+def set_up_database(db_name):
+    """
+    Sets up a SQLite database connection and cursor.
+
+    Parameters
+    -----------------------
+    db_name: str
+        The name of the SQLite database.
+
+    Returns
+    -----------------------
+    Tuple (Cursor, Connection):
+        A tuple containing the database cursor and connection objects.
+    """
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path + "/" + db_name)
+    cur = conn.cursor()
+    return cur, conn
